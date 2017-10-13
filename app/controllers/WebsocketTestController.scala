71ae63636414b5a0f32d5f38c4a8f6fa8b240d94
M S <msokoryansky@yahoo.com>
2017-10-13 09:11:41 -0400

    added static WS test page that pings echo.websocket.org and updated application.conf to allow access to that server in Content-Security-Policy	header

---
+++ app/controllers/WebsocketTestController.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import javax.inject._
+
+import play.api._
+import play.api.mvc._
+
+import scala.concurrent.Future
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+@Singleton
+class WebsocketTestController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.websocket_test())
+  }
+}
