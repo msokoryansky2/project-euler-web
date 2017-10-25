f65ce4f8c1c9fb065679d3fd9761210fa632b215
M S <msokoryansky@yahoo.com>
2017-10-24 20:21:06 -0400

    adding a "help page"

---
+++ public/javascripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(e) {
+    $("#what-is-this").click(function() {
+        $("#overlay").show();
+    });
+
+    $("#got-it").click(function() {
+        $("#overlay").hide();
+    });
+});
