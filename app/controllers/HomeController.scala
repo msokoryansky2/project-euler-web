bc3d2396686999d071b3dab81b05d29d332cea4d
M S <msokoryansky@yahoo.com>
2017-10-28 14:56:15 -0400

    trying to refactor main view into left and right + introduce prod.conf file. All busted

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,8 +2,10 @@ package controllers
 
 import javax.inject._
 
+import play.api.Configuration
 import play.api.mvc._
 import services.EulerProblemService
+
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -12,13 +14,15 @@ import scala.concurrent.ExecutionContext.Implicits.global
  * application's home page which lists Project Euler problems with available solutions
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents,
+                               configuration: Configuration) extends AbstractController(cc) {
   val logger = play.api.Logger(getClass)
+  val googleApiKey: String = configuration.getOptional[String]("google.api.key").getOrElse("N/A")
 
   def index() = Action.async { implicit request: Request[AnyContent] =>
     logger.info(s"Web request for home page")
     Future {
       EulerProblemService.availableProblems
-    } map (problemList => Ok(views.html.index(problemList)))
+    } map (problemList => Ok(views.html.main(views.html.right(problemList))(googleApiKey)))
   }
 }
