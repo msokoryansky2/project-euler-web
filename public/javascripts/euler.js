ff11cef7f91f42804736c8d7f14e9c0ed0d74ce4
M S <msokoryansky@yahoo.com>
2017-11-17 10:12:04 -0500

    tweaks to map event to display who solved

--- public/javascripts/euler.js
+++ public/javascripts/euler.js
@@ -62,5 +62,5 @@ function problemSuccess(id, solution) {
     $("button#problem_" + id).removeClass("in-progress unsolved error").addClass("solved");
 
     // Create a map event for aolutions (mine and others')
-    /* if (solution.isMine <= 0) */ mapEvent("solution", solution.by, "Problem # " + solution.problemNumber + " solved: " + solution.answer);
+    /* if (solution.isMine <= 0) */ mapEvent("solution", solution.by, solution.by.desc + " solved # " + solution.problemNumber);
 }
